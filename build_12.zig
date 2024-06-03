c9d7cf1bbb4ad8aadb0918357febf005686b9def
jinzhongjia <mail@nvimer.org>
2024-06-03 20:02:08 +0800

    remove info

--- build_12.zig
+++ build_12.zig
@@ -19,8 +19,6 @@ pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    log.info("link mode is {s}", .{if (isStatic) "static" else "dynamic"});
-
     if (enableTLS) {
         log.info("enable TLS support", .{});
         if (!target.query.isNative()) {
@@ -41,7 +39,7 @@ pub fn build(b: *Build) !void {
     const webui = try webui_c(b, optimize, target, isStatic, enableTLS);
 
     const webui_module = b.addModule("webui", .{
-        .root_source_file = b.path("src/webui.zig"),
+        .root_source_file = b.path(b.pathJoin(&.{ "src", "webui.zig" })),
         .imports = &.{
             .{
                 .name = "flags",
@@ -95,19 +93,19 @@ fn webui_c(b: *Build, optimize: OptimizeMode, target: Build.ResolvedTarget, is_s
     }
 
     webui.addCSourceFile(.{
-        .file = webui_dep.path("src/webui.c"),
+        .file = webui_dep.path(b.pathJoin(&.{ "src", "webui.c" })),
         .flags = if (enable_tls) &tls_flags else &no_tls_flags,
     });
 
     webui.addCSourceFile(.{
-        .file = webui_dep.path("src/civetweb/civetweb.c"),
+        .file = webui_dep.path(b.pathJoin(&.{ "src", "civetweb", "civetweb.c" })),
         .flags = civetweb_flags.items,
     });
 
     webui.linkLibC();
 
     webui.addIncludePath(webui_dep.path("include"));
-    webui.installHeader(webui_dep.path(b.pathJoin(&[_][]const u8{ "include", "webui.h" })), "webui.h");
+    webui.installHeader(webui_dep.path(b.pathJoin(&.{ "include", "webui.h" })), "webui.h");
 
     // for windows build
     if (target.result.os.tag == .windows) {
@@ -123,7 +121,7 @@ fn webui_c(b: *Build, optimize: OptimizeMode, target: Build.ResolvedTarget, is_s
         }
     } else if (target.result.os.tag == .macos) {
         webui.addCSourceFile(.{
-            .file = webui_dep.path("src/webview/wkwebview.m"),
+            .file = webui_dep.path(b.pathJoin(&.{ "src", "webview", "wkwebview.m" })),
             .flags = &.{},
         });
         webui.linkFramework("Cocoa");
@@ -146,7 +144,7 @@ fn install_webui_c(b: *Build, lib: *Compile) void {
 fn generate_docs(b: *Build, optimize: OptimizeMode, target: Build.ResolvedTarget, flags_module: *Module) void {
     const webui_lib = b.addObject(.{
         .name = "webui_lib",
-        .root_source_file = b.path("src/webui.zig"),
+        .root_source_file = b.path(b.pathJoin(&.{ "src", "webui.zig" })),
         .target = target,
         .optimize = optimize,
     });
@@ -165,6 +163,7 @@ fn generate_docs(b: *Build, optimize: OptimizeMode, target: Build.ResolvedTarget
 }
 
 fn build_examples_12(b: *Build, optimize: OptimizeMode, target: Build.ResolvedTarget, webui_module: *Module, webui_lib: *Compile) void {
+
     // we use lazyPath to get absolute path of package
     var lazy_path = b.path("examples");
 
@@ -185,7 +184,7 @@ fn build_examples_12(b: *Build, optimize: OptimizeMode, target: Build.ResolvedTa
         if (val) |entry| {
             if (entry.kind == .directory) {
                 const example_name = entry.name;
-                const path = b.pathJoin(&[_][]const u8{ "examples", example_name, "main.zig" });
+                const path = b.pathJoin(&.{ "examples", example_name, "main.zig" });
 
                 const exe = b.addExecutable(.{
                     .name = example_name,
@@ -204,7 +203,7 @@ fn build_examples_12(b: *Build, optimize: OptimizeMode, target: Build.ResolvedTa
                 const exe_run = b.addRunArtifact(exe);
                 exe_run.step.dependOn(&exe_install.step);
 
-                const cwd = b.path(b.pathJoin(&[_][]const u8{ "examples", example_name }));
+                const cwd = b.path(b.pathJoin(&.{ "examples", example_name }));
 
                 exe_run.setCwd(cwd);
 
